207a4881e086f51bd1369428354434bafcae89ed
Steffen Weinstock <79531202+stewsk@users.noreply.github.com>
2026-07-10 15:14:27 +0200

    Skeleton plugin (#1)
    * Skeleton plugin
    
    - remove all unused files
    - basic settings
    - basic Readme
    - no content yet
    - add dummy test

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default [
     },
   },
   {
-    files: ["tests/**"],
+    files: ["test/**"],
     rules: {
       "no-console": "off",
       "no-undef": "off",
---
+++ test/dummy.test.js
@@ -0,0 +1,5 @@
+describe("dummy", () => {
+  it("passes", () => {
+    expect(true).toBe(true)
+  })
+})
